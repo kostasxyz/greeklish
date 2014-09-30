3906d81c5695f51f584b81d6cbb1e62cffa4f0f5
Kostas Charal <skapator@gmail.com>
2014-09-30 11:33:54 +0300

    Update Greeklish.php

--- src/Skapator/Greeklish/Greeklish.php
+++ src/Skapator/Greeklish/Greeklish.php
@@ -129,7 +129,7 @@ class Greeklish {
         $text = preg_replace( array('/&.*?;/', '/\s+/', '/[^A-Za-z0-9_\.\-]/u'),  array(' ', '-', ''), $text );
         $text = filter_var(strtolower($text), FILTER_SANITIZE_URL);
 
-        return \Str::lug($text);
+        return \Str::slug($text);
     }
 
     /**
